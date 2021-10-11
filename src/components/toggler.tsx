2eb62dc0df8a3c034658ebd9fc9ce8ad3558d51a
Luigi Morel <morelmiles@gmail.com>
2021-10-11 17:17:47 +0300

    Remove unused vars

--- src/components/toggler.tsx
+++ src/components/toggler.tsx
@@ -15,6 +15,7 @@ export default function Toggler(props: IProps) {
       <input type="checkbox" onChange={onChange} checked={checked} />
       <span className="wrapper" onClick={() => setChecked(!checked)}>
         <span className="knob"></span>
+        <>{label}</>
       </span>
     </div>
   );

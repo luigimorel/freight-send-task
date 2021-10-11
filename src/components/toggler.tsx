4550c2aa40a548dbab2c68585b773e39a98bfbf2
Luigi Morel <morelmiles@gmail.com>
2021-10-11 16:53:52 +0300

    Add a custom selection slider

---
+++ src/components/toggler.tsx
@@ -0,0 +1,21 @@
+import { ChangeEventHandler, useState } from "react";
+
+interface IProps {
+  className?: string;
+  label?: string;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+}
+
+export default function Toggler(props: IProps) {
+  const { className, label, onChange } = props;
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <div className={className}>
+      <input type="checkbox" onChange={onChange} checked={checked} />
+      <span className="wrapper" onClick={() => setChecked(!checked)}>
+        <span className="knob"></span>
+      </span>
+    </div>
+  );
+}
